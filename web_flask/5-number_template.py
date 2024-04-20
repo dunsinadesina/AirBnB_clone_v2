bbdd25c78767f77ff421959d6edaae7ed6d13eb2
dunsinadesina <adesinajesudunsin@gmail.com>
2024-04-20 19:56:07 +0000

    update

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""starts flask application
+the application listens on 0.0.0.0, port 5000
+Routes:
+	/: display "Hello HBNB!"
+	/hbnb: displays "HBNB"
+	/c/<text>: displays 'C' followed by the value for <text>
+	/python/(<text>): displays "Python" followed by the value for <text>
+	/number/n: displays 'n is a number' only if n is an integer
+	/number_template/<n>: displays an HTML page only if n is an integer
+"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	"""display 'Hello HBNB!'"""
+	return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+	"""displays 'HBNB'"""
+	return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+	"""displays 'C' followed by the value for <text>"""
+	text = text.replace("_", " ")
+	return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+	"""displays 'Python' followed by the value for <text>"""
+	text = text.replace("_", " ")
+	return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+	"""displays 'n is a number' only if n is an integer"""
+	return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+	"""displays an HTML page only if n is an integer"""
+	return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
