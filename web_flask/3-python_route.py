2e1b914606f6bc62e5fbfa5ba24974ce7224498f
dunsinadesina <adesinajesudunsin@gmail.com>
2024-04-20 18:59:32 +0000

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""starts a flask application
+
+the application listens on 0.0.0.0, on port 5000
+Routes:
+	/: displays 'Hello HBNB'
+	/hbnb: displays 'HBNB'
+	/c/<text>: displays 'C' followed by the value of <text>
+	/python/(<text>): displays 'Python' followed by the value of <text>
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	"""displays 'Hello HBNB'"""
+	return "Hello HBNB"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+	"""displays 'HBNB'"""
+	return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+	"""displays 'C' followed by the value of <text>"""
+	text = text.replace("_", " ")
+	return "C {}".format(text)
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+	"""displays 'Python' followed by the value of <text>"""
+	text = text.replace("_", ".")
+	return "Python {}".format(text)
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
